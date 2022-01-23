7adbf9e1511e7de6f621a034c9054f6a0e5daab4
hllbr <halibrahim.kocak@gmail.com>
2022-01-23 23:24:30 +0300

    toursList

---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h1>loading...
+      </h1>
+      <h2>＝＝＝＝＝＝＝ᕕ( °0°)ᕗ ♥♥(Ő‿Ő♥)</h2>
+      <h3> @˃̶͈ ֊̫ ˂̶͈@ </h3>
+    </div>
+  );
+};
+
+export default Loading;
